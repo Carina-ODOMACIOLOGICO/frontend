7c339f261269405bc20ebfbd8ad5ebdcfaa175c6
ALEXANDER ENRIQUE TOSCANO RICARDO <atoscano@correo.unicordoba.edu.co>
2022-12-28 09:12:22 -0500

    Fixes named Onomasiologico

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <v-app-bar app color="primary" dark>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Diccionario Onomaciológico</v-toolbar-title>
+      <v-toolbar-title>Diccionario Onomasiológico</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
